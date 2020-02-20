78a9c4d3165e12c6d3a56cc16d51cde88b4623c2
Teerth Patel <teerth1205@gmail.com>
2020-02-20 04:27:40 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 24, "MALE", 'RED', 'ROWDY', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 24, "MALE", 'RED', 'ROWDY', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 24, "MALE", 'RED', 'ROWDY', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 24, "female", 'RED', 'ROWDY', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 24, "MALE", 'RED', 'ROWDY', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 24, "female", 'RED', 'ROWDY', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 24, "MALE", 'RED', 'ROWDY', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 24, "MALE", 'RED', 'ROWDY', 1);
